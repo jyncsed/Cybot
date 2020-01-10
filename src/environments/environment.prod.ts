6e2443f3f3f900b31a10fd85dafe95d60a1c42e7
James <jamesbridenthal@gmail.com>
2020-01-09 19:42:38 -0500

    Fixing images

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  version: '0.0.3',
-  buildDate: 'Thu Jan 09 2020 18:59:57 GMT-0500 (Eastern Standard Time)',
+  version: '0.0.4',
+  buildDate: 'Thu Jan 09 2020 19:41:01 GMT-0500 (Eastern Standard Time)',
   baseAPIUrl: 'https://localhost:44306/'
  };
