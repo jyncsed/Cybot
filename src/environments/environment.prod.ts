b3f3096bef10ad71810ed009733881b1527b81d7
James <jamesbridenthal@gmail.com>
2020-01-11 17:56:01 -0500

    udpate team member card and build. Hopefully fix image as well

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  version: '0.0.6',
-  buildDate: 'Sat Jan 11 2020 17:21:41 GMT-0500 (Eastern Standard Time)',
+  version: '0.0.8',
+  buildDate: 'Sat Jan 11 2020 17:52:39 GMT-0500 (Eastern Standard Time)',
   baseAPIUrl: 'https://localhost:44306/'
  };
