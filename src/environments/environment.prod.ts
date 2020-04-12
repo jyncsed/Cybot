f506965e585de247102dd2fd79b5ce14ba10b03c
James <jamesbridenthal@gmail.com>
2020-04-12 15:55:40 -0400

    update build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  version: '0.0.15',
-  buildDate: 'Wed Mar 25 2020 19:41:58 GMT-0400 (Eastern Daylight Time)',
+  version: '0.0.16',
+  buildDate: 'Sun Apr 12 2020 15:53:22 GMT-0400 (Eastern Daylight Time)',
   baseAPIUrl: 'https://localhost:44306/'
  };
